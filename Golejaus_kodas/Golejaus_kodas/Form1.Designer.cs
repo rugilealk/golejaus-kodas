cdbf9947ba19e80f142f180ee00dbf46683f6858
Rugilė Alkovikaitė <rugilealk555@gmail.com>
2025-11-24 21:49:50 +0200

    Initiated repository

---
+++ Golejaus_kodas/Golejaus_kodas/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Golejaus_kodas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pasirinkimoComboBox = new ComboBox();
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // pasirinkimoComboBox
+            // 
+            pasirinkimoComboBox.FormattingEnabled = true;
+            pasirinkimoComboBox.Location = new Point(300, 197);
+            pasirinkimoComboBox.Name = "pasirinkimoComboBox";
+            pasirinkimoComboBox.Size = new Size(151, 28);
+            pasirinkimoComboBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(330, 240);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(346, 156);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(pasirinkimoComboBox);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox pasirinkimoComboBox;
+        private Button button1;
+        private Label label1;
+    }
+}
---
+++ Golejaus_kodas/Golejaus_kodas/Form1.cs
@@ -0,0 +1,10 @@
+namespace Golejaus_kodas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
